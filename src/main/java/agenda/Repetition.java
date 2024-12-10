08e226b63aed03fc558ede1eb90054e304fb1d4b
antoniofauci <antoniofauci1@gmail.com>
2024-12-10 16:12:57 +0100

    TPAgendaFinal

--- src/main/java/agenda/Repetition.java
+++ src/main/java/agenda/Repetition.java
@@ -1,46 +1,29 @@
 package agenda;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Repetition {
-    public ChronoUnit getFrequency() {
-        return myFrequency;
-    }
 
-    /**
-     * Stores the frequency of this repetition, one of :
-     * <UL>
-     * <LI>ChronoUnit.DAYS for daily repetitions</LI>
-     * <LI>ChronoUnit.WEEKS for weekly repetitions</LI>
-     * <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
-     * </UL>
-     */
     private final ChronoUnit myFrequency;
+    private final Set<LocalDate> exceptions = new HashSet<>();
+    private Termination termination;
 
     public Repetition(ChronoUnit myFrequency) {
         this.myFrequency = myFrequency;
     }
 
-    /**
-     * Les exceptions à la répétition
-     * @param date un date à laquelle l'événement ne doit pas se répéter
-     */
-    public void addException(LocalDate date) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    public ChronoUnit getFrequency() {
+        return myFrequency;
     }
 
-    /**
-     * La terminaison d'une répétition (optionnelle)
-     * @param termination la terminaison de la répétition
-     */
-    public void setTermination(Termination termination) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    public void addException(LocalDate date) {
+        exceptions.add(date);
+    }
 
+    public void setTermination(Termination termination) {
+        this.termination = termination;
     }
 }
